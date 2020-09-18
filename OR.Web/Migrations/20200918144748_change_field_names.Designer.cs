45b737d3b49bdd6c2b9b18f9a46255d12ab531f6
Joseph Nguyen <joseph@teksoft1.com>
2020-09-18 22:02:55 +0700

    abc

--- OR.Web/Migrations/20200918144748_change_field_names.Designer.cs
+++ OR.Web/Migrations/20200918144748_change_field_names.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using OR.Data;
 
 namespace OR.Web.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200918144748_change_field_names")]
+    partial class change_field_names
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ OR.Web/Migrations/20200918144748_change_field_names.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OR.Web.Migrations
+{
+    public partial class change_field_names : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EmailAddress",
+                table: "MembershipRequests");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "MembershipRequests");
+
+            migrationBuilder.RenameColumn(
+                name: "EmiratesIdNumber",
+                table: "MembershipRequests",
+                newName: "EmiratesIDNumber");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "MembershipRequests",
+                maxLength: 250,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FullName",
+                table: "MembershipRequests",
+                maxLength: 250,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "MembershipRequests");
+
+            migrationBuilder.DropColumn(
+                name: "FullName",
+                table: "MembershipRequests");
+
+            migrationBuilder.RenameColumn(
+                name: "EmiratesIDNumber",
+                table: "MembershipRequests",
+                newName: "EmiratesIdNumber");
+
+            migrationBuilder.AddColumn<string>(
+                name: "EmailAddress",
+                table: "MembershipRequests",
+                type: "nvarchar(250)",
+                maxLength: 250,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "MembershipRequests",
+                type: "nvarchar(250)",
+                maxLength: 250,
+                nullable: true);
+        }
+    }
+}
